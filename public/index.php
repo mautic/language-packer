d17c7292eda20bbe1584df1c78c2f51e043f1eeb
Tejas Navghane <ts.navghane@gmail.com>
2023-04-12 03:47:15 +0530

    add symfony 6 and dev packages

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use MauticLanguagePacker\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return static function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+};
