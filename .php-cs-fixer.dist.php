b15af6df23e4ca80b05bba40c78f200f7809aa6f
Tejas Navghane <ts.navghane@gmail.com>
2023-04-19 19:18:51 +0530

    config .php-cs-fixer.dist.php and fixcs

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,30 +1,26 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__.'/src');
+declare(strict_types=1);
 
-return (new PhpCsFixer\Config())
-    ->setRules([
-        '@Symfony'                  => true,
-        'binary_operator_spaces'    => [
+$finder = (new PhpCsFixer\Finder())->in(__DIR__.'/src');
+
+return (new PhpCsFixer\Config())->setRules(
+    [
+        '@Symfony'               => true,
+        'binary_operator_spaces' => [
             'operators' => [
                 '=>' => 'align',
                 '='  => 'align',
             ],
         ],
-        'phpdoc_to_comment'         => false,
-        'ordered_imports'           => true,
-        'array_syntax'              => [
+        'phpdoc_to_comment'      => false,
+        'ordered_imports'        => true,
+        'array_syntax'           => [
             'syntax' => 'short',
         ],
-        'no_unused_imports'         => true,
-        /**
-         * Our templates rely heavily on things like endforeach, endif, etc.
-         * This setting should be turned off at least until we've switched to Twig
-         * (which is required for Symfony 5).
-         */ 'no_alternative_syntax' => false,
-        'header_comment'            => [
+        'no_unused_imports'      => true,
+        'header_comment'         => [
             'header' => '',
         ],
-    ])
-    ->setFinder($finder);
+    ]
+)->setFinder($finder);
