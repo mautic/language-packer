93ad010d4b244e3f471edb2a2b215b58c497ae39
Tejas Navghane <ts.navghane@gmail.com>
2023-04-25 01:47:38 +0530

    Update MauticLanguagePackerCommandTest.php

--- tests/Functional/Command/MauticLanguagePackerCommandTest.php
+++ tests/Functional/Command/MauticLanguagePackerCommandTest.php
@@ -5,111 +5,56 @@ declare(strict_types=1);
 namespace App\Tests\Functional\Command;
 
 use App\Command\MauticLanguagePackerCommand;
-use App\Tests\Common\Client\TransifexTestClient;
-use GuzzleHttp\Psr7\Response;
+use App\Service\BuildPackageService;
+use App\Service\Transifex\ResourcesService;
+use App\Service\UploadPackageService;
+use App\Tests\Common\Client\MockResponse;
+use GuzzleHttp\Handler\MockHandler;
 use PHPUnit\Framework\Assert;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\Request;
 
 class MauticLanguagePackerCommandTest extends KernelTestCase
 {
-    private readonly CommandTester $commandTester;
+    private ?CommandTester $commandTester = null;
 
-    private readonly TransifexTestClient $client;
+    private ?Filesystem $filesystem = null;
 
-    private readonly Filesystem $filesystem;
+    private ?string $packagesDir = null;
 
-    private string $packagesDir;
+    private ?string $translationsDir = null;
 
-    private string $translationsDir;
+    private ?MockHandler $mockHandler = null;
 
     protected function setUp(): void
     {
-        $container       = self::getContainer();
-        $parameterBag    = $container->get('parameter_bag');
-        $eventDispatcher = $container->get('event_dispatcher');
-
-        $this->client = $container->get('transifex.test.client');
+        $container        = self::getContainer();
+        $parameterBag     = $container->get('parameter_bag');
+        $this->filesystem = $container->get('filesystem');
 
         $application = new Application();
-        $application->add(new MauticLanguagePackerCommand($parameterBag, $eventDispatcher));
-        $command             = $application->find('mautic:language:packer');
+        $application->add(
+            new MauticLanguagePackerCommand(
+                $parameterBag,
+                $this->filesystem,
+                $container->get(ResourcesService::class),
+                $container->get(BuildPackageService::class),
+                $container->get(UploadPackageService::class)
+            )
+        );
+        $command             = $application->find(MauticLanguagePackerCommand::NAME);
         $this->commandTester = new CommandTester($command);
-        $this->filesystem    = new Filesystem();
 
         $this->packagesDir     = $parameterBag->get('mlp.packages.dir');
         $this->translationsDir = $parameterBag->get('mlp.translations.dir');
 
+        $this->mockHandler = $container->get('http.client.mock_handler');
+
         $this->backupTestTranslationsFolder();
-    }
-
-    protected function tearDown(): void
-    {
-        $this->restoreTestTranslationsFolder();
-        $this->removeExtraPackagesFolder();
-    }
-
-    public function testExecute(): void
-    {
-        $expectedBody = <<<EOT
-{
-  "id": "o:some_organisation:p:some_project:r:addonbundle-flashes",
-  "type": "resources",
-  "attributes": {
-    "slug": "addonbundle-flashes",
-    "name": "AddonBundle flashes",
-    "priority": "high",
-    "i18n_type": "PHP_INI",
-    "i18n_version": 1,
-    "accept_translations": true,
-    "string_count": 9,
-    "word_count": 59,
-    "datetime_created": "2014-12-02T20:11:50Z",
-    "datetime_modified": "2015-01-12T15:25:59Z",
-    "i18n_options": []
-  },
-  "relationships": {
-    "project": {
-      "links": {
-        "related": "https://rest.api.transifex.com/projects/o:some_organisation:p:some_project"
-      },
-      "data": {
-        "type": "projects",
-        "id": "o:some_organisation:p:some_project"
-      }
-    },
-    "i18n_format": {
-      "data": {
-        "type": "i18n_formats",
-        "id": "PHP_INI"
-      }
-    }
-  },
-  "links": {
-    "self": "https://rest.api.transifex.com/resources/o:some_organisation:p:some_project:r:addonbundle-flashes"
-  }
-}
-EOT;
-        $this->prepareSuccessTest(200, [], $expectedBody);
-        $this->commandTester->execute(['--upload-package' => true]);
-
-        Assert::assertStringContainsString(
-            '[OK] Successfully created language packages for Mautic!',
-            $this->getFixedCommandOutput()
-        );
-    }
-
-    private function getFixedCommandOutput(): string
-    {
-        return preg_replace('/  +/', ' ', str_replace(PHP_EOL, '', $this->commandTester->getDisplay()));
-    }
-
-    private function prepareSuccessTest(int $status = 200, array $headers = [], $body = null): void
-    {
-        $this->client->setResponse(new Response($status, $headers, $body));
     }
 
     private function backupTestTranslationsFolder(): void
@@ -120,11 +65,25 @@ EOT;
         $this->filesystem->mkdir($this->translationsDir);
     }
 
+    protected function tearDown(): void
+    {
+        $this->restoreTestTranslationsFolder();
+        $this->removeExtraPackagesFolder();
+    }
+
     private function restoreTestTranslationsFolder(): void
     {
         $translationsBckDir = $this->getTranslationsBackupDir();
         $this->filesystem->remove($this->translationsDir);
         $this->filesystem->rename($translationsBckDir, $this->translationsDir);
+    }
+
+    private function getTranslationsBackupDir(): string
+    {
+        $pathParts = explode('/', $this->translationsDir);
+        array_pop($pathParts);
+
+        return implode('/', $pathParts).'/translations-bck';
     }
 
     private function removeExtraPackagesFolder(): void
@@ -143,11 +102,132 @@ EOT;
         }
     }
 
-    private function getTranslationsBackupDir(): string
+    public function testExecute(): void
     {
-        $pathParts = explode('/', $this->translationsDir);
-        array_pop($pathParts);
+        $organisation = $_ENV['TRANSIFEX_ORGANISATION'];
+        $project      = $_ENV['TRANSIFEX_PROJECT'];
 
-        return implode('/', $pathParts).'/translations-bck';
+        $slug     = 'addonbundle-flashes';
+        $resource = 'AddonBundle flashes';
+        $language = 'af';
+
+        $headers = $this->getCommonHeaders();
+
+        $resourcesExpectedBody = <<<EOT
+{
+  "data": [
+    {
+      "attributes": {
+        "slug": "$slug",
+        "name": "$resource"
+      }
+    }
+  ]
+}
+EOT;
+        $resourcesUri = "https://rest.api.transifex.com/resources?filter%5Bproject%5D=o%3A$organisation%3Ap%3A$project";
+
+        $statisticsExpectedBody = <<<EOT
+{
+  "data": [
+    {
+      "id": "o:mautic:p:mautic:r:$resource:l:$language",
+      "attributes": {
+        "last_update": "2015-05-21T08:06:10Z"
+      }
+    }
+  ]
+}
+EOT;
+        $statisticsUri = "https://rest.api.transifex.com/resource_language_stats?filter%5Bresource%5D=o%3A$organisation%3Ap%3A$project%3Ar%3A$slug&filter%5Bproject%5D=o%3A$organisation%3Ap%3A$project";
+
+        $uuid                     = 'ab267026-4109-44ef-a13f-3c369d0e8a3c';
+        $translationsExpectedBody = <<<EOT
+{
+  "data": {
+    "id": "$uuid",
+    "links": {
+      "self": "https://rest.api.transifex.com/resource_translations_async_downloads/$uuid"
+    }
+  }
+}
+EOT;
+        $translationsUri = 'https://rest.api.transifex.com/resource_translations_async_downloads';
+
+        $translations2ExpectedBody = 'mautic.addon.notice.reloaded="%added% addons were added, %updated% updated, and %disabled% disabled."';
+        $translations2Uri          = 'https://rest.api.transifex.com/resource_translations_async_downloads/ab267026-4109-44ef-a13f-3c369d0e8a3c';
+
+        $languagesExpectedBody = <<<EOT
+{
+  "data": {
+    "attributes": {
+      "code": "af",
+      "name": "Afrikaans"
+    }
+  }
+}
+EOT;
+        $languagesUri = 'https://rest.api.transifex.com/languages/l%3Aaf';
+
+        $this->mockHandler->append(
+            $this->getMockResponse(
+                $resourcesExpectedBody,
+                Request::METHOD_GET,
+                $resourcesUri,
+                $headers
+            ),
+            $this->getMockResponse(
+                $statisticsExpectedBody,
+                Request::METHOD_GET,
+                $statisticsUri,
+                $headers
+            ),
+            $this->getMockResponse(
+                $translationsExpectedBody,
+                Request::METHOD_POST,
+                $translationsUri,
+                array_merge(['Content-Length' => ['498']], $headers)
+            ),
+            $this->getMockResponse(
+                $translations2ExpectedBody,
+                Request::METHOD_GET,
+                $translations2Uri,
+                $headers
+            ),
+            $this->getMockResponse(
+                $languagesExpectedBody,
+                Request::METHOD_GET,
+                $languagesUri,
+                $headers
+            )
+        );
+
+        $this->commandTester->execute([]);
+
+        Assert::assertStringContainsString('[OK] Successfully created language packages for Mautic!', $this->getFixedCommandOutput());
+    }
+
+    private function getCommonHeaders(): array
+    {
+        return [
+            'User-Agent'    => ['GuzzleHttp/7'],
+            'Host'          => ['rest.api.transifex.com'],
+            'accept'        => ['application/vnd.api+json'],
+            'content-type'  => ['application/vnd.api+json'],
+            'authorization' => ['Bearer not-a-real-api-token'],
+        ];
+    }
+
+    private function getMockResponse(string $body, string $method, string $uri, array $headers): MockResponse
+    {
+        return MockResponse::fromString($body)
+            ->assertRequestMethod($method)
+            ->assertRequestUri($uri)
+            ->assertRequestHeaders($headers);
+    }
+
+    private function getFixedCommandOutput(): string
+    {
+        return preg_replace('/  +/', ' ', str_replace(PHP_EOL, '', $this->commandTester->getDisplay()));
     }
 }
