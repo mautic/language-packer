b2c447a2c51e32e60e18501993b64fcb937726e8
Tejas Navghane <ts.navghane@gmail.com>
2023-04-27 21:19:46 +0530

    Update S3ClientFactory.php

--- tests/Common/Client/S3ClientFactory.php
+++ tests/Common/Client/S3ClientFactory.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace App\Tests\Common\Client;
 
+use App\Aws\S3Config;
 use Aws\MockHandler;
 use Aws\S3\S3Client;
 use Aws\S3\S3ClientInterface;
 
 class S3ClientFactory
 {
-    public static function create(MockHandler $handler): S3ClientInterface
+    public static function create(MockHandler $handler, S3Config $config): S3ClientInterface
     {
         return new S3Client([
-            'version'     => $_ENV['AWS_S3_VERSION'],
-            'region'      => $_ENV['AWS_S3_REGION'],
+            'version'     => $config->version,
+            'region'      => $config->region,
             'credentials' => [
                 'key'    => '',
                 'secret' => '',
